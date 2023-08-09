8d0444e27119adbba0277189c8e0b142e02766b2
Yinghao Wang <yinghao.a.wang@gmail.com>
2023-08-09 12:21:40 -0700

    add: tailwind & react-router scaffold

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div className='text-3xl font-bold underline'>Hello world!</div>
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
