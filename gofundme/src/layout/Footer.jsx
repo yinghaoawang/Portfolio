8b7fd10181259446144efc5d2fd7d62f95c2c7d0
Yinghao Wang <yinghao.a.wang@gmail.com>
2023-08-19 18:11:56 -0700

    add locale picker button w/ us flag

--- gofundme/src/layout/Footer.jsx
+++ gofundme/src/layout/Footer.jsx
@@ -8,7 +8,7 @@ const TopFooter = () => {
           </a>
         </div>
         <div className='flex flex-col gap-3'>
-          <span className='font-bold'>Fundraise for</span>
+          <span className='font-bold text-gray'>Fundraise for</span>
           {[
             { title: 'Medical' },
             { title: 'Emergency' },
@@ -24,7 +24,7 @@ const TopFooter = () => {
           ))}
         </div>
         <div className='flex flex-col gap-3'>
-          <span className='font-bold'>Learn more</span>
+          <span className='font-bold text-gray'>Learn more</span>
           {[
             { title: 'How GoFundMe Works' },
             { title: 'Why GoFundMe' },
@@ -40,7 +40,7 @@ const TopFooter = () => {
           ))}
         </div>
         <div className='flex flex-col gap-3'>
-          <span className='font-bold'>Resources</span>
+          <span className='font-bold text-gray'>Resources</span>
           {[
             { title: 'Help Center' },
             { title: 'Blog' },
@@ -63,17 +63,26 @@ const TopFooter = () => {
 const BottomFooter = () => {
   const currentYear = new Date().getFullYear();
   return (
-    <div className='inner-container mx-auto'>
-      <div className='pb-4 bg-[var(--footer-bg)] text-[var(--footer-text)]'>
-        <div className='flex flex-col items-start'>
-          <button className='bg-gray-300'>Locale Picker</button>
-          <div className='flex flex-wrap gap-6'>
-            <span>©{currentYear} Yinghao Wang</span>
-            <a>Terms</a>
-            <a>Privacy Notice</a>
-            <a>Do Not Sell My Personal Information</a>
-            <a>Legal</a>
-            <a>Accessibility Statement</a>
+    <div className='inner-container mx-auto mb-8 mt-2'>
+      <div className='flex pb-4 bg-[var(--footer-bg)] text-[var(--footer-text)]'>
+        <div className='flex flex-col items-start gap-y-4'>
+          <button className='border border-[var(--lighter-gray)] rounded-md shadow-md flex px-2 py-2'>
+            <img className='my-auto object-cover w-5 h-5 rounded-full' src='/us-flag-icon.svg' />
+            <span className='ml-3'>United States<span className='mx-2'>·</span>English</span>
+          </button>
+          <div className='flex flex-wrap gap-x-6 gap-y-2'>
+            <span className='text-gray'>©{currentYear} Yinghao Wang</span>
+            {[
+              { title: 'Terms' },
+              { title: 'Privacy Notice' },
+              { title: 'Do Not Sell My Personal Information' },
+              { title: 'Legal' },
+              { title: 'Accessibility Statement' }
+            ].map((data) => (
+              <a href='#' className='link-normal text-light'>
+                {data.title}
+              </a>
+            ))}
           </div>
         </div>
         <div className='shrink-0 flex w-[300px]'>
